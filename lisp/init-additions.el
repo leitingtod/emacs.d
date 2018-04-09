12b4ca9c977999ce105dac53b28e56d86ebd50f2
liuyazhou <leitingtod@126.com>
2018-04-09 10:15:32 +0800

    添加 eyebrowse，管理多个窗口布局

--- lisp/init-additions.el
+++ lisp/init-additions.el
@@ -6,6 +6,8 @@
   (interactive)
   (term-send-raw-string "aykj83752661\n"))
 
+;; 在multi-term打开的ssh连接中，使用vi等编辑器非常地难用
+;; 好像没什么好的解决办法
 (use-package multi-term
   :ensure
   :defer t
@@ -22,5 +24,29 @@
   (add-hook 'term-mode-hook
 			(lambda () (define-key term-raw-map (kbd "C-c p") 'term-send-ssh-passwd))))
 
+;;smartparens
+;;flycheck
+;;hydra
+;;unicad
+;;yasnippet
+;;magit
+;;cnfonts
+;;multi-cursors
+;;smartparens
+;;winner-mode
+;;percol
+
+(use-package eyebrowse
+  :ensure t
+  :defer t
+;;  :diminish eyebrowse-mode
+  :config
+  (progn
+	(setq eyebrowse-new-workspace t)
+	(define-key eyebrowse-mode-map (kbd "M-1") 'eyebrowse-switch-to-window-config-1)
+	(define-key eyebrowse-mode-map (kbd "M-2") 'eyebrowse-switch-to-window-config-2)
+	(define-key eyebrowse-mode-map (kbd "M-3") 'eyebrowse-switch-to-window-config-3)
+	(define-key eyebrowse-mode-map (kbd "M-4") 'eyebrowse-switch-to-window-config-4)))
+(eyebrowse-mode t)
 
 (provide 'init-additions)
