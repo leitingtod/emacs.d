711dc5496c956b2ce6f6f87b4143644fb467c9c1
leiting <leitingtod@gmail.com>
2019-07-24 16:46:26 +0800

    add org-mode

--- init.el
+++ init.el
@@ -42,17 +42,3 @@
 (message
  (format "Emacs startup time: %.3f seconds."
          (float-time (time-since emacs-start-time))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (cnfonts org-bullets org-plus-contrib whitespace-cleanup-mode indent-guide persp-mode spacemacs-theme spaceline nyan-mode flx counsel smex diminish use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
